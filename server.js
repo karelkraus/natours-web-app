4d16c23642f9e09f50746a7d88e4777df89ed2f3
karelkraus <kaja.kraus@gmail.com>
2021-03-28 09:40:06 +0200

    added heroku config

--- server.js
+++ server.js
@@ -32,3 +32,10 @@ process.on('unhandledRejection', (err) => {
   console.log(err.name, err.message);
   server.close(() => process.exit(1));
 });
+
+process.on('SIGTERM', () => {
+  console.log('SIGTERM received. Shutting down 😎');
+  server.close(() => {
+    console.log('🙄 Process terminated!');
+  });
+});
